81f810e201bbde785cb580d9792a98dcdddc075e
Saraswat Sahoo <ssahoo.code.13@gmail.com>
2024-08-05 03:19:54 +0530

    trail

---
+++ script.js
@@ -0,0 +1,36 @@
+let images = Array.from(document.getElementsByClassName("imgCarousel"));
+let mainPhoto = document.getElementById("mainPhoto");
+let currentIndex = 0;
+
+function updateImage(event){
+  let image = event.target;
+  setActiveThumbnail(image);
+  mainPhoto.src = image.src;
+}
+
+images.forEach(function(image){
+  image.addEventListener("click", updateImage);
+});
+
+function setActiveThumbnail(activeImage) {
+  images.forEach(function(image) {
+    image.classList.remove("active");
+  });
+  activeImage.classList.add("active");
+}
+
+function showNextImage() {
+  mainPhoto.style.opacity = 0; // Start fade out
+  setTimeout(() => {
+    currentIndex = (currentIndex + 1) % images.length;
+    mainPhoto.src = images[currentIndex].src;
+    setActiveThumbnail(images[currentIndex]);
+    mainPhoto.style.opacity = 1; // Fade in
+  }, 500); // Duration of the fade out
+}
+
+// Set the interval to change images every 3 seconds (3000 milliseconds)
+setInterval(showNextImage, 3000);
+
+// Set the initial active thumbnail
+setActiveThumbnail(images[currentIndex]);
